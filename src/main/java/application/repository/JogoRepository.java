bfcd56e284beae06b77c4bae35f5ddcb59cb0f75
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-11-07 17:45:58 +0000

    save

---
+++ src/main/java/application/repository/JogoRepository.java
@@ -0,0 +1,10 @@
+package application.repository;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import application.model.Jogo;
+ 
+public interface JogoRepository extends JpaRepository<Jogo, Long> {
+   
+}
+ 
